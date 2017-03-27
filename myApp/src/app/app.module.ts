caa3a8b329e1500a0b699912cab7f6d7c1a02c87
Kleyson Morais <kleysonb@hotmail.com>
2017-03-27 16:30:17 -0600

    Conexão com o Banco de Dados Firebase

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -9,6 +9,18 @@ import { LoginPage } from '../pages/login/login';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+
+// AF2 Settings
+export const firebaseConfig = {
+   apiKey: "AIzaSyDPpBVfyDkEskpromg8Sy1zpoc02ZyjJCE",
+   authDomain: "candler-a7b00.firebaseapp.com",
+   databaseURL: "https://candler-a7b00.firebaseio.com",
+   storageBucket: "candler-a7b00.appspot.com",
+   messagingSenderId: "532455404591"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,7 +30,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     TabsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- myApp/src/pages/progresso/progresso.ts
+++ myApp/src/pages/progresso/progresso.ts
@@ -1,22 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 
-/*
-  Generated class for the Progresso page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-progresso',
   templateUrl: 'progresso.html'
 })
 export class ProgressoPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  teste: FirebaseListObservable<any>;
+  constructor(public navCtrl: NavController, public navParams: NavParams, af: AngularFire) {
+    this.teste = af.database.list("/teste");
+    console.log(this.teste);
+    //console.log(this.teste.nome/);
+  }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProgressoPage');
+    //console.log('ionViewDidLoad ProgressoPage');
   }
 
 }
